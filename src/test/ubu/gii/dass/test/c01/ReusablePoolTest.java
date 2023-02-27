20d5233b04283ced21d8f6061a4401ea9120d4cd
Adripy <adri_cordero89@hotmail.com>
2023-02-27 19:10:28 +0100

    Implementación Test AcquireReusable
    Caso de Prueba: AdquirirReusable.
    - Descripción: Se llama al método acquireReusable para obtener 3 instancias de la clase Reusable. Prueba que el valor devuelto no es nulo y que pertenecen a la clase Reusable. Se comprueba que las 2 primeras son objetos distintos y el último lanzará la excepción por ser nulo que será capturada y se comprobara que es null .- Entrada: Ninguna .- Salida Esperada: Devuelve verdadero.

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -15,7 +15,7 @@ import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
 
 /**
- * @author 
+ * @author Cordero Bernal, Adrian y Garcia Armario, Jesus
  *
  */
 public class ReusablePoolTest {
@@ -55,7 +55,32 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testAcquireReusable() {
-		fail("Not yet implemented");
+		// Inicializamos
+		Reusable object1, object2, object3 = null; 
+		
+		try {
+			// Asignamos el primero
+			object1 = pool.acquireReusable();
+			// Comprobamos que no sea nulo
+			assertNotNull(object1);	
+			// Comprobamos que es una instancia de Reusable
+			assertTrue(object1 instanceof Reusable);
+			
+			// Asignamos el primero
+			object2 = pool.acquireReusable();
+			// Comprobamos que no sea nulo
+			assertNotNull(object2);
+			// Comprobamos que es una instancia de Reusable
+			assertTrue(object2 instanceof Reusable);
+			// Comprobamos que no sean el mismo objeto
+			assertFalse(object1.hashCode() == object2.hashCode());	
+			
+			// Solicitamos un tercero inexistente para forzar NotFreeInstanceException
+			object3 = pool.acquireReusable(); 
+			
+		} catch (NotFreeInstanceException e) {
+			assertNull(object3);
+		}
 	}
 
 	/**
