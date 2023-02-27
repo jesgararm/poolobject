9f87000cd5f6707649820b86028e24eabfd7b09b
Adripy <adri_cordero89@hotmail.com>
2023-02-27 18:51:33 +0100

    Correcciones y documentación
    -implementación de metodos setUp() y tearDown().
    -agregados autores.
    -se importan las clases del resto de métodos faltantes.

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -4,22 +4,30 @@
 package ubu.gii.dass.test.c01;
 
 import static org.junit.Assert.*;
-import ubu.gii.dass.c01.ReusablePool;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.DuplicatedInstanceException;
+import ubu.gii.dass.c01.NotFreeInstanceException;
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
+
 /**
- * @author alumno
+ * @author 
  *
  */
 public class ReusablePoolTest {
-
+	
+	private ReusablePool pool;
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
 	public void setUp() throws Exception {
+		pool = ReusablePool.getInstance();
 	}
 
 	/**
@@ -27,6 +35,7 @@ public class ReusablePoolTest {
 	 */
 	@After
 	public void tearDown() throws Exception {
+		pool = null;
 	}
 
 	/**
