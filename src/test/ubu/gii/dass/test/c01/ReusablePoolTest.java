ff285510106aa298f4bc6f5c7787aa83363e16a8
Jesús GA <94868006+jesgararm@users.noreply.github.com>
2023-02-27 19:26:45 +0100

    Implementación test ReleaseReusable
    Caso de Prueba: ReleaseReusable.
    - Descripción: Se llama al método acquireReusable para obtener una instancia de la clase Reusable, almacenar su hashID y posteriormente liberarla con releaseReusable. Se adquiere otra instancia de Reusable, y comprobamos que los objetos anterior y actual coinciden.
    Liberamos dos veces, forzando el lanzamiento de la excepción "DuplicatedInstanceException"
     .- Entrada: Ninguna .
    - Salida Esperada: Devuelve verdadero.

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -19,9 +19,9 @@ import ubu.gii.dass.c01.ReusablePool;
  *
  */
 public class ReusablePoolTest {
-	
+
 	private ReusablePool pool;
-	
+
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -50,22 +50,23 @@ public class ReusablePoolTest {
 		// Comprobamos que es una instancia de ReusablePool
 		assertTrue(pool instanceof ReusablePool);
 	}
+
 	/**
 	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
 	 */
 	@Test
 	public void testAcquireReusable() {
 		// Inicializamos
-		Reusable object1, object2, object3 = null; 
-		
+		Reusable object1, object2, object3 = null;
+
 		try {
 			// Asignamos el primero
 			object1 = pool.acquireReusable();
 			// Comprobamos que no sea nulo
-			assertNotNull(object1);	
+			assertNotNull(object1);
 			// Comprobamos que es una instancia de Reusable
 			assertTrue(object1 instanceof Reusable);
-			
+
 			// Asignamos el primero
 			object2 = pool.acquireReusable();
 			// Comprobamos que no sea nulo
@@ -73,22 +74,37 @@ public class ReusablePoolTest {
 			// Comprobamos que es una instancia de Reusable
 			assertTrue(object2 instanceof Reusable);
 			// Comprobamos que no sean el mismo objeto
-			assertFalse(object1.hashCode() == object2.hashCode());	
-			
+			assertFalse(object1.hashCode() == object2.hashCode());
+
 			// Solicitamos un tercero inexistente para forzar NotFreeInstanceException
-			object3 = pool.acquireReusable(); 
-			
+			object3 = pool.acquireReusable();
+
 		} catch (NotFreeInstanceException e) {
 			assertNull(object3);
 		}
 	}
 
 	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * Test method for
+	 * {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+	 * 
+	 * @throws NotFreeInstanceException
 	 */
 	@Test
-	public void testReleaseReusable() {
-		fail("Not yet implemented");
-	}
+	public void testReleaseReusable() throws NotFreeInstanceException {
+		Reusable object1, object2 = null; // Creamos tres objetos vacíos
+		int hashId = 0;
+		try {
+			object1 = pool.acquireReusable(); // Solicitamos el primero
+			hashId = object1.hashCode(); // Guardamos su identificador
+			pool.releaseReusable(object1); // Liberamos el objeto
+			object2 = pool.acquireReusable(); // Solicitamos el segundo
+			assertTrue(hashId == object2.hashCode()); // Comprobamos que coinciden 
+			pool.releaseReusable(object2); // Liberamos el objet
+			pool.releaseReusable(object2); // Forzamos la excepción DuplicatedInstanceException
+		} catch (DuplicatedInstanceException e) {
+			assertTrue(hashId == object2.hashCode()); // Comprobamos que efectivamente es la misma instancia
+		}
 
+	}
 }
